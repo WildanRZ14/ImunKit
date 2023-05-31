c8876d73515277aad59e61cba00f4a67bba105f2
muflih11 <20sa1103@mhs.amikompurwokerto.ac.id>
2023-05-31 15:20:02 +0700

    Menambahkan tampilan landing page

--- app/Views/index.php
+++ app/Views/index.php
@@ -33,6 +33,86 @@
       </div>
     </nav>
   </section>
+  <section id="landingPage" class="landingPage">
+  <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="home-content" data-aos="fade-up" data-aos-duration="1000">
+                        <h1 class="text-home-bold fw-bold text-primary mt-1">
+                        Selamat Datang di ImunKit</h1>
+                        <h4 class="text-home-reguler fw-normal text-secondary">
+                        Solusi tepat untuk menjaga kesehatan buah hati Anda. Anak sehat dan bebas penyakit dengan imunisasi tepat waktu.
+                        </h4>
+                        <div class="home-btn mt-5">
+                            <a href="#" class="btn btn-primary shadow-none">Lihat Lebih Lanjut</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="home-img" data-aos="fade-up" data-aos-duration="2000">
+                        <img src="<?= base_url('assets/images/LandingPage.png'); ?>" class="w-100" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+  </section>
+  <section class="fitur" id="fitur">
+  <div class="container pt-3">
+            <h3 class="fw-bold text-primary mt-3 text-center">Fitur</h3>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 content mt-5" data-aos="fade-up" data-aos-duration="1000">
+                  <div class="text-center">
+                    <img src="<?= base_url('assets/images/guide.png'); ?>" class="w-50" alt="">
+                  </div>
+                    <h5 class="services-title text-dark mt-4">Pemantauan</h5>
+                    <p class="text-secondary mt-3">Membantu orang tua dalam memantau perkembangan bayi
+                    </p>
+                </div>
+                <div class="col-lg-4 col-md-6 content mt-5" data-aos="fade-up" data-aos-duration="2000">
+                <div class="text-center">
+                    <img src="<?= base_url('assets/images/timetable.png'); ?>" class="w-50" alt="">
+                  </div>
+                    <h5 class="services-title text-dark mt-4">Jadwal Imunisasi</h5>
+                    <p class="text-secondary mt-3">Memberi tahu orang tua seputar jadwal imunisasi yang akan diadakan.
+                    </p>
+                </div>
+                <div class="col-lg-4 col-md-6 content mt-5" data-aos="fade-up" data-aos-duration="3000">
+                <div class="text-center">
+                    <img src="<?= base_url('assets/images/website.png'); ?>" class="w-50" alt="">
+                  </div>
+                    <h5 class="services-title text-dark mt-4">Pendaftaran Imunisasi</h5>
+                    <p class="text-secondary mt-3">Orang tua melakukan pendaftaran untuk mendapatkan informasi seputar imunisasi</p>
+                </div>
+            </div>
+        </div>
+  </section>
+  <section class="about" id="about">
+        <div class="container">
+        <h3 class="fw-bold text-primary mt-5 text-center pt-5">Tentang Kami</h3>
+            <div class="row align-items-center pb-3">
+                <div class="col-lg-6 col-md-12">
+                    <div class="row">
+                        <div class="col-md-11">
+                            <div class="gallery-img mt-3 text-center" data-aos="fade-up" data-aos-duration="1000">
+                                <img src="<?= base_url('assets/images/contact.png'); ?>" class="w-75" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="about-content" data-aos="fade-up" data-aos-duration="2000">
+                        <h2 class="fw-bold text-dark mt-3">ImunKit</h2>
+                        <p class="text-secondary mt-3 text-justify">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, 
+                            saepe quod molestiae minima eaque modi atque culpa sit libero ducimus
+                             magnam nobis numquam id alias eveniet vel, animi esse ut perspiciatis
+                            laborum! Doloremque inventore beatae unde officia quos culpa quis?
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
   <script src="<?= base_url(); ?>/assets/style/js/bootstrap.bundle.min.js"></script>
