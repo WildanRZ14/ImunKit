d38896e5efc80df22b079fdd58e96422d30b2262
josephineprb <josephineprb@gmail.com>
2023-06-19 23:15:02 +0700

    Menambahkan meta

--- app/Views/register.php
+++ app/Views/register.php
@@ -2,6 +2,7 @@
 <html lang="id">
 
 <head>
+    <meta name="dicoding:email" content="syifanamuflih06@gmail.com">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
